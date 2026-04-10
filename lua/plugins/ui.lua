fd95df4ae25df47fe3a8ff8eda9622030003305b
pxy1337 <pxy1337@tutanota.de>
2026-04-10 23:52:34 +0200

    Add search info

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -71,4 +71,23 @@ require('lualine').setup({
     extensions = { 'neo-tree' },
 })
 
+--
+-- Experimental UI
+--
 require('vim._core.ui2').enable({})
+
+--
+-- Search Info
+--
+vim.pack.add({ 'https://github.com/kevinhwang91/nvim-hlslens' }, { confirm = false })
+require('hlslens').setup()
+local kopts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
